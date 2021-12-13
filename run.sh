59872de6a1d6437e7603c80263e89268c0e28734
daghlani <daghlani@behsazan.net>
2021-12-13 17:14:03 +0330

    [bug fix] avoid to overwrite config file.

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cp /app/exp/config.yml /app/config/
+EXP_FILE=$(md5sum exp/config.yml | awk '{print $1}')
+CFG_FILE=$(md5sum config/config.yml | awk '{print $1}')
+if [[ $EXP_FILE == $CFG_FILE ]]; then
+  echo cp /app/exp/config.yml /app/config/
+fi
 bash /start.sh
 
