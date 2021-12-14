3da92c9286d2b94d43cf396bcbb4f74851f0bb8e
a.daghlani <a.daghlani@raveshmand.com>
2021-12-14 23:27:03 +0330

    bugfix copy config file

--- run.sh
+++ run.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-EXP_FILE=$(md5sum exp/config.yml | awk '{print $1}')
-CFG_FILE=$(md5sum config/config.yml | awk '{print $1}')
-if [[ $EXP_FILE == $CFG_FILE ]]; then
-  echo cp /app/exp/config.yml /app/config/
+EXP_FILE=$(md5sum /app/exp/config.yml | awk '{print $1}')
+CFG_FILE=$(md5sum /app/config/config.yml | awk '{print $1}')
+if [[ $EXP_FILE == $CFG_FILE ]] || [[ $(ls /app/config/config.yml 2> /dev/null) = '' ]]; then
+	cp /app/exp/config.yml /app/config/
 fi
 bash /start.sh
-
